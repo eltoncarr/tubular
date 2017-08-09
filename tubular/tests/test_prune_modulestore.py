6c7e38b51a645fd11b648831a13480691c9552d5
Elton.C <eltonc@microsoft.com>
2017-08-09 13:22:47 -0700

    WIP - Enabling test case & fixing error with main
    	modified:   tubular/tests/test_prune_modulestore.py

--- tubular/tests/test_prune_modulestore.py
+++ tubular/tests/test_prune_modulestore.py
@@ -3,7 +3,6 @@ Tests for pruning modulestore.
 """
 
 from __future__ import absolute_import
-from __future__ import unicode_literals
 
 import json
 import logging
@@ -11,17 +10,43 @@ import os
 import sys
 import unittest
 
-import ddt
+import tubular.scripts.prune_modulestore as pruning_script
 
-@ddt.ddt
+
 class TestModuleStorePruning(unittest.TestCase):
     u"""
     Test basic modulestore pruning
     """
-    def testPrune(self): 
-        self.assertEqual((True, True)
-    
+
+    # output file for post-prune data
+    output_file = u"output.json"
+
+    # input file: static dataset
+    input_file = u"test_prune_modulestore_data.json"
+
+    def removeOutputFile(self):
+        # check if the specified file exists
+        file_exists = os.path.isfile(self.output_file)
+
+        assert isinstance(file_exists, object)
+        if file_exists:
+            # there is residual output
+            os.remove(self.output_file)
+
+    def setUp(self):
+        # make sure we remove any residual output file
+        # this is defensive
+        self.removeOutputFile()
+
+    def tearDown(self):
+        # make sure we remove any residual output fileimport tubular.jenkins as jenkins
+        self.removeOutputFile()
+
+    def testPrune(self):
+        
+        # stub
+        self.assertEqual(True, True)
+
 
 if __name__ == '__main__':
     unittest.main()
-
